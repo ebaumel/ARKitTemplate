a6723fd4a0dc2a5415c6fc735a3dfb16c9d337d6
Eric M. Baumel <ebaumel@softcode.com>
2023-05-19 13:09:59 -0400

    Initial commit.

---
+++ ARKitTemplate/ARKitTemplate/ARKitTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitTemplateApp.swift
+//  ARKitTemplate
+//
+//  Created by Eric Baumel on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ARKitTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# ARKitTemplate
+
+## A starter template for Apple ARKit apps.
+
+Copyright 2023 Softcode Systems, LLC. All rights reserved.
